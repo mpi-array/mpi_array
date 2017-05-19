5310e5882cbef79e701f56d4e24a9197cdaa7108
Shane-J-Latham <Shane-J-Latham@users.noreply.github.com>
2017-05-19 14:39:05 +1000

    More implementation in mpi_array.decomposition.

--- mpi_array/decomposition_test.py
+++ mpi_array/decomposition_test.py
@@ -37,12 +37,14 @@ __license__ = _license()
 __copyright__ = _copyright()
 __version__ = _mpi_array.__version__
 
+
 class MemNodeTopologyTest(_unittest.TestCase):
     """
     :obj:`unittest.TestCase` for :mod:`mpi_array.decomposition.MemNodeTopology`.
     """
 
     def testConstructInvalidDims(self):
+        mnt = None
         with self.assertRaises(ValueError):
             mnt = MemNodeTopology()
         with self.assertRaises(ValueError):
@@ -50,9 +52,11 @@ class MemNodeTopologyTest(_unittest.TestCase):
         with self.assertRaises(ValueError):
             mnt = MemNodeTopology(dims=tuple(), ndims=1)
         with self.assertRaises(ValueError):
-            mnt = MemNodeTopology(dims=tuple([0,2]), ndims=1)
+            mnt = MemNodeTopology(dims=tuple([0, 2]), ndims=1)
         with self.assertRaises(ValueError):
-            mnt = MemNodeTopology(dims=tuple([1,2]), ndims=3)
+            mnt = MemNodeTopology(dims=tuple([1, 2]), ndims=3)
+
+        self.assertEqual(None, mnt)
 
     def testConstructShared(self):
         mnt = MemNodeTopology(ndims=1)
@@ -70,12 +74,26 @@ class MemNodeTopologyTest(_unittest.TestCase):
         mnt = MemNodeTopology(dims=(0, 0, 0))
         self.assertEqual(_mpi.IDENT, _mpi.Comm.Compare(_mpi.COMM_WORLD, mnt.rank_comm))
 
+    def testConstructNoShared(self):
+        mnt = MemNodeTopology(ndims=1, shared_mem_comm=_mpi.COMM_SELF)
+        self.assertEqual(_mpi.IDENT, _mpi.Comm.Compare(_mpi.COMM_WORLD, mnt.rank_comm))
+        self.assertEqual(1, mnt.shared_mem_info.shared_mem_comm.size)
+
+
 class DecompositionTest(_unittest.TestCase):
     """
     :obj:`unittest.TestCase` for :mod:`mpi_array.decomposition.Decomposition`.
     """
-    def testConstructInvalidDims(self):
-        pass
+
+    def testConstruct(self):
+        """
+        Test :obj:`mpi_array.decomposition.Decomposition` construction.
+        """
+        decomp = Decomposition((8 * _mpi.COMM_WORLD.size,))
+        self.assertNotEqual(None, decomp.mem_node_topology)
+
+        decomp = \
+            Decomposition((8 * _mpi.COMM_WORLD.size,))
 
 _unittest.main(__name__)
 
