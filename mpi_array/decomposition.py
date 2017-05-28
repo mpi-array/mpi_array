c448e9bf5678c9f00b4b237e353a3b571812519d
Shane-J-Latham <Shane-J-Latham@users.noreply.github.com>
2017-05-28 16:53:33 +1000

    More mpi_array.decomposition implementation.

--- mpi_array/decomposition.py
+++ mpi_array/decomposition.py
@@ -12,6 +12,7 @@ Classes and Functions
    :toctree: generated/
 
    IndexingExtent - Index range for a tile of a decomposition.
+   HaloIndexingExtent - Index range, including ghost elements, for a tile of a decomposition.
    DecompExtent - Indexing and halo info for a tile in a cartesian decomposition.
    SharedMemInfo - Shared-memory communicator generation.
    MemNodeTopology - Topology of MPI processes which allocate shared memory.
@@ -219,28 +220,28 @@ if (_sys.version_info[0] >= 3) and (_sys.version_info[1] >= 5):
 
 class IndexingExtent(object):
     """
-    Index bounding box for a single tile of domain decomposition.
+    Indexing bounds for a single tile of domain decomposition.
     """
 
-    def __init__(self, split, halo=None):
+    def __init__(self, split=None, start=None, stop=None):
         """
         Construct.
 
         :type split: sequence of :obj:`slice`
         :param split: Per axis start and stop indices defining the extent.
-        :type halo: :samp:`(len({split}), 2)` shaped array of :obj:`int`
-        :param halo: A :samp:`(len(self.start), 2)` shaped array of :obj:`int` indicating the
-           per-axis number of outer ghost elements. :samp:`halo[:,0]` is the number
-           of elements on the low-index *side* and :samp:`halo[:,1]` is the number of
-           elements on the high-index *side*.
+        :type start: sequence of :obj:`int`
+        :param start: Per axis *start* indices defining the start of extent.
+        :type stop: sequence of :obj:`int`
+        :param stop: Per axis *stop* indices defining the extent.
 
         """
         object.__init__(self)
-        self._beg = _np.array([s.start for s in split])
-        self._end = _np.array([s.stop for s in split])
-        if halo is None:
-            halo = _np.zeros((self.beg_.shape[0], 2), dtype=self._beg.dtype)
-        self._halo = halo
+        if split is not None:
+            self._beg = _np.array([s.start for s in split], dtype="int64")
+            self._end = _np.array([s.stop for s in split], dtype=self._beg.dtype)
+        elif (start is not None) and (stop is not None):
+            self._beg = _np.array(start, dtype="int64")
+            self._end = _np.array(stop, dtype="int64")
 
     @property
     def start(self):
@@ -259,16 +260,6 @@ class IndexingExtent(object):
         return self._end
 
     @property
-    def halo(self):
-        """
-        A :samp:`(len(self.start), 2)` shaped array of :obj:`int` indicating the
-        per-axis number of outer ghost elements. :samp:`halo[:,0]` is the number
-        of elements on the low-index *side* and :samp:`halo[:,1]` is the number of
-        elements on the high-index *side*.
-        """
-        return self._end
-
-    @property
     def shape(self):
         """
         Sequence of :obj:`int` indicating the shape of this extent
@@ -276,13 +267,123 @@ class IndexingExtent(object):
         """
         return self._end - self._beg
 
+    @property
+    def ndim(self):
+        """
+        Dimension of indexing.
+        """
+        return self._beg.size
 
-class DecompExtent(IndexingExtent):
+    def calc_intersection(self, other):
+        """
+        Returns the indexing extent which is the intersection of
+        this extent with the :samp:`{other}` extent.
+
+        :type other: :obj:`IndexingExtent`
+        :param other: Perform intersection calculation using this extent.
+        :rtype: :obj:`IndexingExtent`
+        :return: :samp:`None` if the extents do not intersect, otherwise
+           returns the extent of overlapping indices.
+        """
+        intersection_extent = \
+            IndexingExtent(
+                start=_np.maximum(self._beg, other._beg),
+                stop=_np.minimum(self._end, other._end)
+            )
+        if _np.any(intersection_extent._beg >= intersection_extent._end):
+            intersection_extent = None
+
+        return intersection_extent
+
+
+class HaloIndexingExtent(IndexingExtent):
+    """
+    Indexing bounds, including ghost elements, for a single tile of domain decomposition.
+    """
+
+    #: The "low index" indices.
+    LO = 0
+
+    #: The "high index" indices.
+    HI = 1
+
+    def __init__(self, split, halo=None):
+        """
+        Construct.
+
+        :type split: sequence of :obj:`slice`
+        :param split: Per axis start and stop indices defining the extent.
+        :type halo: :samp:`(len({split}), 2)` shaped array of :obj:`int`
+        :param halo: A :samp:`(len(self.start), 2)` shaped array of :obj:`int` indicating the
+           per-axis number of outer ghost elements. :samp:`halo[:,0]` is the number
+           of elements on the low-index *side* and :samp:`halo[:,1]` is the number of
+           elements on the high-index *side*.
+
+        """
+        IndexingExtent.__init__(self, split)
+        if halo is None:
+            halo = _np.zeros((self.beg_.shape[0], 2), dtype=self._beg.dtype)
+        self._halo = halo
+
+    @property
+    def halo(self):
+        """
+        A :samp:`(len(self.start), 2)` shaped array of :obj:`int` indicating the
+        per-axis number of outer ghost elements. :samp:`halo[:,0]` is the number
+        of elements on the low-index *side* and :samp:`halo[:,1]` is the number of
+        elements on the high-index *side*.
+        """
+        return self._halo
+
+    @property
+    def start_h(self):
+        """
+        The start index of the tile with "halo" elements.
+        """
+        return self._beg
+
+    @property
+    def start_n(self):
+        """
+        The start index of the tile without "halo" elements ("no halo").
+        """
+        return self._beg + self._halo[:, self.LO]
+
+    @property
+    def stop_h(self):
+        """
+        The stop index of the tile with "halo" elements.
+        """
+        return self._end
+
+    @property
+    def stop_n(self):
+        """
+        The stop index of the tile without "halo" elements ("no halo").
+        """
+        return self._end - self._halo[:, self.HI]
+
+    @property
+    def shape_h(self):
+        """
+        The shape of the tile with "halo" elements.
+        """
+        return self._end - self._beg
+
+    @property
+    def shape_n(self):
+        """
+        The stop index of the tile without "halo" elements ("no halo").
+        """
+        return self._end - self._halo[:, self.HI] - self._beg - self._halo[:, self.LO]
+
+
+class DecompExtent(HaloIndexingExtent):
     """
     Indexing extents for single tile of cartesian domain decomposition.
     """
 
-    def __init__(self, cart_coord, cart_shape, slice, halo, bounds_policy=ARRAY_BOUNDS):
+    def __init__(self, cart_rank, cart_coord, cart_shape, slice, halo, bounds_policy=ARRAY_BOUNDS):
         """
         Construct.
 
@@ -298,19 +399,216 @@ class DecompExtent(IndexingExtent):
            of elements on the low-index *side* and :samp:`halo[:,1]` is the number of
            elements on the high-index *side*.
         """
+        self._cart_rank = cart_rank
+        self._cart_coord = _np.array(cart_coord, dtype="int64")
+        self._cart_shape = _np.array(cart_shape, dtype=self._cart_coord.dtype)
+        halo = _np.array(halo)
         if (bounds_policy == ARRAY_BOUNDS):
             # Make the halo
             halo *= \
-                _np.where(
-                    _np.logical_and(
-                        (cart_coord - 1) >= 0,
-                        (cart_coord + 1) < cart_shape
-                    ),
-                    1,
-                    0
-                ).reshape((halo.shape[0], 1))
+                _np.array(
+                    (
+                        _np.where(
+                            (self._cart_coord - 1) >= 0,
+                            1,
+                            0
+                        ),
+                        _np.where(
+                            (self._cart_coord + 1) < self._cart_shape,
+                            1,
+                            0
+                        ),
+                    )
+                ).reshape((halo.shape[0], 2))
 
-        IndexingExtent.__init__(self, slice, halo)
+        HaloIndexingExtent.__init__(self, slice, halo)
+
+    @property
+    def cart_rank(self):
+        """
+        MPI rank of the process in the cartesian decomposition.
+        """
+        return self._cart_rank
+
+    @property
+    def cart_coord(self):
+        """
+        Cartesian coordinate of cartesian decomposition.
+        """
+        return self._cart_coord
+
+    @property
+    def cart_shape(self):
+        """
+        Shape of cartesian decomposition (number of tiles along each axis).
+        """
+        return self._cart_shape
+
+    def halo_slab_extent(self, axis, dir):
+        """
+        Returns indexing extent of the halo *slab* for specified axis.
+
+        :type axis: :obj:`int`
+        :param axis: Indexing extent of halo slab for this axis.
+        :type dir: :attr:`LO` or :attr:`HI`
+        :param dir: Indicates low-index halo slab or high-index halo slab.
+        :rtype: :obj:`IndexingExtent`
+        :return: Indexing extent for halo slab.
+        """
+        start = self._beg.copy()
+        stop = self._end.copy()
+        if dir == self.LO:
+            stop[axis] = start[axis] + self._halo[axis, self.LO]
+        else:
+            start[axis] = stop[axis] - self._halo[axis, self.HI]
+
+        return \
+            IndexingExtent(
+                start=start,
+                stop=stop
+            )
+
+    def no_halo_extent(self, axis):
+        """
+        Returns the indexing extent identical to this extent, except
+        has the halo trimmed from the axis specified by :samp:`{axis}`.
+
+        :type axis: :obj:`int`
+        :param axis: Axis for which halo is trimmed.
+        :rtype: :obj:`IndexingExtent`
+        :return: Indexing extent with halo trimmed from axis :samp:`{axis}`.
+        """
+        start = self._beg.copy()
+        start[axis] += self._halo[axis, self.LO]
+        stop = self._end.copy()
+        stop[axis] -= self._halo[axis, self.HI]
+
+        return \
+            IndexingExtent(
+                start=start,
+                stop=stop
+            )
+
+
+class SingleExtentUpdate(object):
+    """
+    Source and destination indexing info for updating the whole of an extent.
+    """
+
+    def __init__(self, dst_extent, src_extent, update_extent):
+        self._dst_extent = dst_extent  #: whole tile of destination rank
+        self._src_extent = src_extent  #: whole tile of source rank
+        self._update_extent = update_extent  #: portion from source required for update
+
+
+class HalosUpdate(object):
+    """
+    Indexing info for updating the halo regions of a single tile
+    on MPI rank :samp:`self.dst_rank`.
+    """
+
+    #: The "low index" indices.
+    LO = HaloIndexingExtent.LO
+
+    #: The "high index" indices.
+    HI = HaloIndexingExtent.HI
+
+    def __init__(self, dst_rank, rank_to_extents_dict):
+        """
+        Construct.
+
+        :type dst_rank: :obj:`int`
+        :param dst_rank: The MPI rank (:samp:`cart_comm`) of the MPI
+           process which is to receive the halo updates.
+        :type rank_to_extents_dict: :obj:`dict`
+        :param rank_to_extents_dict: Dictionary of :samp:`(r, extent)`
+           pairs for all ranks :samp:`r` (of :samp:`cart_comm`), where :samp:`extent`
+           is a :obj:`DecompExtent` object indicating the indexing extent
+           (tile) on MPI rank :samp:`r.`
+        """
+        self.initialise(dst_rank, rank_to_extents_dict)
+
+    def calc_halo_intersection(self, dst_extent, src_extent, axis, dir):
+        """
+        Calculates the intersection of :samp:`{dst_extent}` halo slab with
+        the update region of :samp:`{src_extent}`.
+
+        :type dst_extent: :obj:`DecompExtent`
+        :param dst_extent: Halo slab indicated by :samp:`{axis}` and :samp:`{dir}`
+           taken from this extent.
+        :type src_extent: :obj:`DecompExtent`
+        :param src_extent: This extent, minus the halo in the :samp:`{axis}` dimension,
+           is intersected with the halo slab.
+        :type axis: :obj:`int`
+        :param axis: Axis dimension indicating slab.
+        :type dir: :attr:`LO` or :attr:`HI`
+        :param dir: :attr:`LO` for low-index slab or :attr:`HI` for high-index slab.
+        :rtype: :obj:`IndexingExtent`
+        :return: Overlap extent of :samp:{dst_extent} halo-slab and
+           the :samp:`{src_extent}` update region.
+        """
+        return \
+            dst_extent.halo_slab_extent(axis, dir).calc_intersection(
+                src_extent.no_halo_extent(axis)
+            )
+
+    def split_extent_for_max_elements(self, extent, max_elements=None):
+        """
+        Partitions the specified extent into smaller extents with number
+        of elements no more than :samp:`{max_elements}`.
+
+        :type extent: :obj:`DecompExtent`
+        :param extent: The extent to be split.
+        :type max_elements: :obj:`int`
+        :param max_elements: Each partition of the returned split has no more
+           than this many elements.
+        :rtype: :obj:`list` of :obj:`DecompExtent`
+        :return: List of extents forming a partition of :samp:`{extent}`
+           with each extent having no more than :samp:`{max_element}` elements.
+        """
+        return [extent, ]
+
+    def initialise(self, dst_rank, rank_to_extents_dict):
+        """
+        Calculates the ranks and regions required to update the
+        halo regions of the :samp:`dst_rank` MPI rank.
+
+        :type dst_rank: :obj:`int`
+        :param dst_rank: The MPI rank (:samp:`cart_comm`) of the MPI
+           process which is to receive the halo updates.
+        :type rank_to_extents_dict: :obj:`dict`
+        :param rank_to_extents_dict: Dictionary of :samp:`(r, extent)`
+           pairs for all ranks :samp:`r` (of :samp:`cart_comm`), where :samp:`extent`
+           is a :obj:`DecompExtent` object indicating the indexing extent
+           (tile) on MPI rank :samp:`r.`
+        """
+        self._dst_rank = dst_rank
+        self._dst_extent = rank_to_extents_dict[dst_rank]
+        self._updates = [[[], []]] * self._dst_extent.ndim
+        cart_coord_to_extents_dict = \
+            {
+                tuple(rank_to_extents_dict[r].cart_coord): rank_to_extents_dict[r]
+                for r in rank_to_extents_dict.keys()
+            }
+        for dir in [self.LO, self.HI]:
+            for a in range(self._dst_extent.ndim):
+                if dir == self.LO:
+                    i_range = range(-1, -self._dst_extent.cart_coord[a] - 1, -1)
+                else:
+                    i_range = \
+                        range(1, self._dst_extent.cart_shape[a] - self._dst_extent.cart_coord[a], 1)
+                for i in i_range:
+                    src_cart_coord = _np.array(self._dst_extent.cart_coord, copy=True)
+                    src_cart_coord[a] += i
+                    src_extent = cart_coord_to_extents_dict[tuple(src_cart_coord)]
+                    halo_extent = self.calc_halo_intersection(self._dst_extent, src_extent, a, dir)
+                    if halo_extent is not None:
+                        self._updates[a][dir] += \
+                            self.split_extent_for_max_elements(
+                                SingleExtentUpdate(self._dst_extent, src_extent, halo_extent)
+                        )
+                    else:
+                        break
 
 
 class Decomposition(object):
@@ -373,19 +671,26 @@ class Decomposition(object):
 
         self._shape_decomp = shape_splitter.calculate_split()
 
+        self._cart_rank_to_extents_dict = None
+        self._halo_updates_dict = None
         if self.have_valid_cart_comm:
             cart_dims = _np.array(self.cart_comm.dims)
             self._cart_rank_to_extents_dict = dict()
+            self._halo_updates_dict = dict()
             for cart_rank in range(0, self.cart_comm.size):
                 cart_coords = _np.array(self.cart_comm.Get_coords(cart_rank))
                 self._cart_rank_to_extents_dict[cart_rank] = \
                     DecompExtent(
+                        cart_rank=cart_rank,
                         cart_coord=cart_coords,
                         cart_shape=cart_dims,
                         slice=self._shape_decomp[tuple(cart_coords)],
                         halo=self._halo,
                         bounds_policy=shape_splitter.tile_bounds_policy
                     )  # noqa: E123
+            for cart_rank in range(0, self.cart_comm.size):
+                self._halo_updates_dict[cart_rank] = \
+                    HalosUpdate(cart_rank, self._cart_rank_to_extents_dict)
 
     def __str__(self):
         """
--- mpi_array/decomposition_test.py
+++ mpi_array/decomposition_test.py
@@ -15,8 +15,9 @@ Classes
 .. autosummary::
    :toctree: generated/
 
-   MemNodeTopologyTest - Test case for :mod:`mpi_array.decomposition.MemNodeTopology`.
-   DecompositionTest - Test case for :mod:`mpi_array.decomposition.Decomposition`.
+   IndexingExtentTest - Tests for :obj:`mpi_array.decomposition.IndexingExtent`.
+   MemNodeTopologyTest - Tests for :obj:`mpi_array.decomposition.MemNodeTopology`.
+   DecompositionTest - Tests for :obj:`mpi_array.decomposition.Decomposition`.
 
 
 """
@@ -28,7 +29,7 @@ import mpi_array as _mpi_array
 
 import mpi4py.MPI as _mpi
 import numpy as _np  # noqa: E402,F401
-from mpi_array.decomposition import MemNodeTopology, Decomposition
+from mpi_array.decomposition import IndexingExtent, DecompExtent, MemNodeTopology, Decomposition
 
 __author__ = "Shane J. Latham"
 __license__ = _license()
@@ -36,9 +37,137 @@ __copyright__ = _copyright()
 __version__ = _mpi_array.__version__
 
 
+class IndexingExtentTest(_unittest.TestCase):
+    """
+    :obj:`unittest.TestCase` for :obj:`mpi_array.decomposition.IndexingExtentTest`.
+    """
+
+    def testAttributes(self):
+        """
+        Tests :attr:`mpi_array.decomposition.IndexingExtent.start`
+        and :attr:`mpi_array.decomposition.IndexingExtent.stop`
+        and :attr:`mpi_array.decomposition.IndexingExtent.shape`
+        attributes.
+        """
+        ie = IndexingExtent(start=(10,), stop=(32,))
+        self.assertTrue(_np.all(ie.shape == (22,)))
+        self.assertTrue(_np.all(ie.start == (10,)))
+        self.assertTrue(_np.all(ie.stop == (32,)))
+
+        ie = IndexingExtent((slice(10, 32),))
+        self.assertTrue(_np.all(ie.shape == (22,)))
+        self.assertTrue(_np.all(ie.start == (10,)))
+        self.assertTrue(_np.all(ie.stop == (32,)))
+
+        ie = IndexingExtent(start=(10, 25), stop=(32, 55))
+        self.assertTrue(_np.all(ie.shape == (22, 30)))
+        self.assertTrue(_np.all(ie.start == (10, 25)))
+        self.assertTrue(_np.all(ie.stop == (32, 55)))
+
+        ie = IndexingExtent((slice(10, 32), slice(25, 55)))
+        self.assertTrue(_np.all(ie.shape == (22, 30)))
+        self.assertTrue(_np.all(ie.start == (10, 25)))
+        self.assertTrue(_np.all(ie.stop == (32, 55)))
+
+    def testIntersection1d(self):
+        """
+        Tests :meth:`mpi_array.decomposition.IndexingExtent.calc_intersection` method, 1D indexing.
+        """
+        ie0 = IndexingExtent(start=(10,), stop=(32,))
+        iei = ie0.calc_intersection(ie0)
+        self.assertTrue(_np.all(iei.shape == (22,)))
+        self.assertTrue(_np.all(iei.start == (10,)))
+        self.assertTrue(_np.all(iei.stop == (32,)))
+
+        ie1 = IndexingExtent(start=(5,), stop=(32,))
+        iei = ie0.calc_intersection(ie1)
+        self.assertTrue(_np.all(iei.shape == (22,)))
+        self.assertTrue(_np.all(iei.start == (10,)))
+        self.assertTrue(_np.all(iei.stop == (32,)))
+
+        ie1 = IndexingExtent(start=(10,), stop=(39,))
+        iei = ie0.calc_intersection(ie1)
+        self.assertTrue(_np.all(iei.shape == (22,)))
+        self.assertTrue(_np.all(iei.start == (10,)))
+        self.assertTrue(_np.all(iei.stop == (32,)))
+
+        ie1 = IndexingExtent(start=(-5,), stop=(39,))
+        iei = ie0.calc_intersection(ie1)
+        self.assertTrue(_np.all(iei.shape == (22,)))
+        self.assertTrue(_np.all(iei.start == (10,)))
+        self.assertTrue(_np.all(iei.stop == (32,)))
+
+        ie1 = IndexingExtent(start=(11,), stop=(31,))
+        iei = ie0.calc_intersection(ie1)
+        self.assertTrue(_np.all(iei.shape == (20,)))
+        self.assertTrue(_np.all(iei.start == (11,)))
+        self.assertTrue(_np.all(iei.stop == (31,)))
+
+        ie1 = IndexingExtent(start=(5,), stop=(10,))
+        iei = ie0.calc_intersection(ie1)
+        self.assertEqual(None, iei)
+
+        ie1 = IndexingExtent(start=(32,), stop=(55,))
+        iei = ie0.calc_intersection(ie1)
+        self.assertEqual(None, iei)
+
+
+class DecompExtentTest(_unittest.TestCase):
+    """
+    :obj:`unittest.TestCase` for :obj:`mpi_array.decomposition.DecompExtent`.
+    """
+
+    def testConstructAttribs(self):
+        """
+        """
+        de = \
+            DecompExtent(
+                cart_rank=0,
+                cart_coord=(0,),
+                cart_shape=(1,),
+                slice=(slice(0, 100),),
+                halo=((10, 10),)
+            )
+
+        self.assertEqual(0, de.cart_rank)
+        self.assertTrue(_np.all(de.cart_coord == (0,)))
+        self.assertTrue(_np.all(de.cart_shape == (1,)))
+        self.assertTrue(_np.all(de.halo == 0))
+
+    def testExtentCalcs(self):
+        """
+        """
+        de = \
+            [
+                DecompExtent(
+                    cart_rank=r,
+                    cart_coord=(r,),
+                    cart_shape=(3,),
+                    slice=(slice(r * 100, r * 100 + 100),),
+                    halo=((10, 10),)
+                )
+                for r in range(0, 3)
+            ]
+
+        self.assertEqual(0, de[0].cart_rank)
+        self.assertTrue(_np.all(de[0].cart_coord == (0,)))
+        self.assertTrue(_np.all(de[0].cart_shape == (3,)))
+        self.assertTrue(_np.all(de[0].halo == ((0, 10),)))
+
+        self.assertEqual(1, de[1].cart_rank)
+        self.assertTrue(_np.all(de[1].cart_coord == (1,)))
+        self.assertTrue(_np.all(de[1].cart_shape == (3,)))
+        self.assertTrue(_np.all(de[1].halo == ((10, 10),)))
+
+        self.assertEqual(2, de[2].cart_rank)
+        self.assertTrue(_np.all(de[2].cart_coord == (2,)))
+        self.assertTrue(_np.all(de[2].cart_shape == (3,)))
+        self.assertTrue(_np.all(de[2].halo == ((10, 0),)))
+
+
 class MemNodeTopologyTest(_unittest.TestCase):
     """
-    :obj:`unittest.TestCase` for :mod:`mpi_array.decomposition.MemNodeTopology`.
+    :obj:`unittest.TestCase` for :obj:`mpi_array.decomposition.MemNodeTopology`.
     """
 
     def testConstructInvalidDims(self):
@@ -81,10 +210,10 @@ class MemNodeTopologyTest(_unittest.TestCase):
 
 class DecompositionTest(_unittest.TestCase):
     """
-    :obj:`unittest.TestCase` for :mod:`mpi_array.decomposition.Decomposition`.
+    :obj:`unittest.TestCase` for :obj:`mpi_array.decomposition.Decomposition`.
     """
 
-    def testConstruct1D(self):
+    def testConstruct1d(self):
         """
         Test :obj:`mpi_array.decomposition.Decomposition` construction.
         """
@@ -96,9 +225,27 @@ class DecompositionTest(_unittest.TestCase):
             Decomposition((8 * _mpi.COMM_WORLD.size,), mem_node_topology=mnt)
 
         root_logger = _logging.get_root_logger(self.id(), comm=decomp.rank_comm)
+        root_logger.info("START " + self.id())
         root_logger.info(str(decomp))
+        root_logger.info("END   " + self.id())
 
-    def testConstruct2D(self):
+    def testConstruct1dWithHalo(self):
+        """
+        Test :obj:`mpi_array.decomposition.Decomposition` construction.
+        """
+        decomp = Decomposition((8 * _mpi.COMM_WORLD.size,), halo=((2, 4),))
+        self.assertNotEqual(None, decomp._mem_node_topology)
+
+        mnt = MemNodeTopology(ndims=1, shared_mem_comm=_mpi.COMM_SELF)
+        decomp = \
+            Decomposition((8 * _mpi.COMM_WORLD.size,), halo=((2, 4),), mem_node_topology=mnt)
+
+        root_logger = _logging.get_root_logger(self.id(), comm=decomp.rank_comm)
+        root_logger.info("START " + self.id())
+        root_logger.info(str(decomp))
+        root_logger.info("END   " + self.id())
+
+    def testConstruct2d(self):
         """
         Test :obj:`mpi_array.decomposition.Decomposition` construction.
         """
@@ -113,7 +260,33 @@ class DecompositionTest(_unittest.TestCase):
             )
 
         root_logger = _logging.get_root_logger(self.id(), comm=decomp.rank_comm)
+        root_logger.info("START " + self.id())
         root_logger.info(str(decomp))
+        root_logger.info("END   " + self.id())
+
+    def testConstruct2dWithHalo(self):
+        """
+        Test :obj:`mpi_array.decomposition.Decomposition` construction.
+        """
+        decomp = \
+            Decomposition(
+                (8 * _mpi.COMM_WORLD.size, 12 * _mpi.COMM_WORLD.size),
+                halo=((2, 2), (4, 4))
+            )
+        self.assertNotEqual(None, decomp._mem_node_topology)
+
+        mnt = MemNodeTopology(ndims=2, shared_mem_comm=_mpi.COMM_SELF)
+        decomp = \
+            Decomposition(
+                (8 * _mpi.COMM_WORLD.size, 12 * _mpi.COMM_WORLD.size),
+                halo=((1, 2), (3, 4)),
+                mem_node_topology=mnt
+            )
+
+        root_logger = _logging.get_root_logger(self.id(), comm=decomp.rank_comm)
+        root_logger.info("START " + self.id())
+        root_logger.info(str(decomp))
+        root_logger.info("END   " + self.id())
 
 
 _unittest.main(__name__)
