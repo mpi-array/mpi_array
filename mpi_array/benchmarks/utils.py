277508408f3542cea368bf9eedcd7092537656d0
Shane-J-Latham <Shane-J-Latham@users.noreply.github.com>
2017-10-25 09:05:54 +1100

    Fix instancemethod pickling used in mpi_array.benchmarks.benchmark for python-2.

--- mpi_array/benchmarks/utils.py
+++ mpi_array/benchmarks/utils.py
@@ -10,7 +10,36 @@ See the `LICENSE <https://github.com/spacetelescope/asv/blob/master/LICENSE.rst>
 """
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+import sys as _sys
+
 __license__ = "https://github.com/spacetelescope/asv/blob/master/LICENSE.rst"
+
+if _sys.version_info[0] <= 2:
+    # Work around lack of pickling of instance methods in python-2
+    from copy_reg import pickle as _pickle
+    from types import MethodType as _MethodType
+
+    def _pickle_method(method):
+        func_name = method.im_func.__name__
+        obj = method.im_self
+        cls = method.im_class
+        if func_name.startswith('__') and not func_name.endswith('__'):
+            cls_name = cls.__name__.lstrip('_')
+            if cls_name:
+                func_name = '_' + cls_name + func_name
+        return _unpickle_method, (func_name, obj, cls)
+
+    def _unpickle_method(func_name, obj, cls):
+        for cls in cls.mro():
+            try:
+                func = cls.__dict__[func_name]
+            except KeyError:
+                pass
+            else:
+                break
+        return func.__get__(obj, cls)
+
+    _pickle(_MethodType, _pickle_method, _unpickle_method)
 
 
 def create_linux_process_time():
