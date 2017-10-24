a8a9482cfa52b7a1a1ade9b13e75760e56b28405
Shane-J-Latham <Shane-J-Latham@users.noreply.github.com>
2017-10-25 10:31:43 +1100

    Moved import statements inside '__main__' test conditional in mpi_array.benchmarks.__main__.

--- mpi_array/benchmarks/__main__.py
+++ mpi_array/benchmarks/__main__.py
@@ -11,11 +11,7 @@ for command line options.
 
 """
 from __future__ import absolute_import
-import sys as _sys
-import logging as _builtin_logging
 from ..license import license as _license, copyright as _copyright, version as _version
-from . import benchmark as _benchmark
-from .. import logging as _logging
 
 __author__ = "Shane J. Latham"
 __license__ = _license()
@@ -23,6 +19,11 @@ __copyright__ = _copyright()
 __version__ = _version()
 
 if __name__ == '__main__':
+    import sys as _sys
+    import logging as _builtin_logging
+    from . import benchmark as _benchmark
+    from .. import logging as _logging
+
     _sys.path.pop(0)
     _logging.initialise_loggers(["mpi_array.benchmarks.benchmark"], log_level=_builtin_logging.INFO)
     _benchmark.run_main(_sys.argv)
